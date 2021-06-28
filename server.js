f81ad13a97d09e3b91fdc26f457af1df339a19af
kirtandudhatra <kirtandudhatra.28@gmail.com>
2021-06-28 13:45:53 -0300

    port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes/');
 const response = require('./middlewares/response');
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }), bodyParser.json(), response);
